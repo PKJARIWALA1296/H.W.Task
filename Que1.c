61ccf22ff440513afbb7efa7fd339dbf8947ce4d
JARIWALA PRATIK KISHORBHAI <130529667+PKJARIWALA1296@users.noreply.github.com>
2023-04-27 18:54:08 +0530

    Add files via upload

---
+++ Que1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+	printf(" * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+	printf("\n *                                                     *");
+	printf("\n *            My Name is Pratik Jariwala.              *");
+	printf("\n *      My Date of Birth is 12th January 1996.         *");
+	printf("\n *               I Am 27yr Old.                        *");
+	printf("\n *   My Address is Palanpur Patiya Rander Road, Surat. *");
+	printf("\n *                                                     *");
+	printf("\n * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+}
